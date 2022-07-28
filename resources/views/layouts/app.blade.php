d6a8d8655eec1c66723cbeaeb072e097ee4cdb2d
Arkaanafzaal <Arkaanafzaal@gmail.com>
2022-07-28 09:09:41 +0700

    tampilan

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -31,25 +31,7 @@
                     <span class="navbar-toggler-icon"></span>
                 </button>
 
-                <div class="collapse navbar-collapse" id="navbarSupportedContent">
-                    <!-- Left Side Of Navbar -->
-                    <nav class="navbar navbar-expand-lg">
-                        <div class="container-fluid">
-                          <a class="navbar-brand" href="#">Navbar</a>
-                          <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
-                            <span class="navbar-toggler-icon"></span>
-                          </button>
-                          <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
-                            <div class="navbar-nav">
-                              <a class="nav-link active" aria-current="page" href="#">Home</a>
-                              <a class="nav-link" href="#">Features</a>
-                              <a class="nav-link" href="#">Pricing</a>
-                              <a class="nav-link disabled">Disabled</a>
-                            </div>
-                          </div>
-                        </div>
-                      </nav>
-
+                
                     <!-- Right Side Of Navbar -->
                     <ul class="navbar-nav ms-auto">
                         <!-- Authentication Links -->
